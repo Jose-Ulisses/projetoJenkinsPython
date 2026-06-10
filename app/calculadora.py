6a193d2e88e821784b59d81ec382aff3dcc17c47
Jose-Ulisses <joseulissesbuenoo@gmail.com>
2026-06-10 15:30:54 -0300

    minor change

--- app/calculadora.py
+++ app/calculadora.py
@@ -10,5 +10,5 @@ class Calculadora:
 
     def dividir(self, a, b):
         if b == 0:
-            raise ValueError("Não é possível dividir por zero.")
+            raise ValueError("Não é possível dividir por zero!")
         return a / b
